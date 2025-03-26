f19e676087b2fab5cc9ee07d795ad5cbb7963742
leodoneDev <leodone125@gmail.com>
2025-03-27 01:02:01 +0700

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
                 );
               }
             },
-            child: const Text("Open WhatsApp"),
+            child: const Text("Run WhatsApp"),
           ),
         ),
       ),
